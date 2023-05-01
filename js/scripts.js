d01f2884add11b566327b8898e15c3c3511dca4a
Austn07 <austin.ring.12@student.hlg.edu>
2023-04-30 21:59:30 -0500

    comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/*!
+/*! THIS IS THE TEMPLATE THAT I USED!!
 * Start Bootstrap - Business Casual v7.0.9 (https://startbootstrap.com/theme/business-casual)
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
