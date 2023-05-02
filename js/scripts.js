fe5a89867a6f0236cca7621bb190ab4f9ede17a2
Austn07 <austin.ring.12@student.hlg.edu>
2023-05-02 00:59:40 -0500

    stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-/*! THIS IS THE TEMPLATE THAT I USED!!
+/* THIS IS THE TEMPLATE THAT I USED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 * Start Bootstrap - Business Casual v7.0.9 (https://startbootstrap.com/theme/business-casual)
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
 */
-// Highlights current date on contact page!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Switches to highlight current day on contact page - Identify and comment use of a switch statement in code
 window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
